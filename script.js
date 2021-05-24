5eb9a334efcef922dc74547a20d3bf72b8a57a13
superbe <babarykin@list.ru>
2021-05-25 03:19:15 +0500

    +

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Определяем отступы для диаграммы
+var margin = { top: 20, right: 90, bottom: 30, left: 90 },
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+
+// Добавляем объект svg в тело страницы.
+// Добавляем элемент 'group' к 'svg' и перемещаем 
+// элемент 'group' к верхнему левому краю.
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.right + margin.left)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate("
+		+ margin.left + "," + margin.top + ")");
+
+var i = 0, duration = 750, root;
+
+// Объявляем древовидную структуру и назначаем размер.
+var treemap = d3.tree().size([height, width]);
+
 fetch('flare-2.json').then(function (response) {
 	if (response.ok) {
 		response.json().then(function (json) {
@@ -19,6 +39,7 @@ fetch('flare-2.json').then(function (response) {
 		console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
 	}
 });
+
 var treeData =
 {
 	"name": "Top Level",
@@ -33,28 +54,6 @@ var treeData =
 		{ "name": "Level 2: B" }
 	]
 };
-
-// Set the dimensions and margins of the diagram
-var margin = { top: 20, right: 90, bottom: 30, left: 90 },
-	width = 960 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-	.attr("width", width + margin.right + margin.left)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", "translate("
-		+ margin.left + "," + margin.top + ")");
-
-var i = 0,
-	duration = 750,
-	root;
-
-// declares a tree layout and assigns the size
-var treemap = d3.tree().size([height, width]);
 
 // Assigns parent, children, height, depth
 root = d3.hierarchy(treeData, function (d) { return d.children; });
