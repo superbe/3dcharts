3cd63d89ec93f0077506d7ec384c1c53ce217865
superbe <babarykin@list.ru>
2021-05-25 03:04:24 +0500

    +

--- script.js
+++ script.js
@@ -13,40 +13,39 @@
 //	]
 //};
 
-function main(treeData) {
+var treeData = {}
 
-	// Set the dimensions and margins of the diagram
-	var margin = { top: 20, right: 90, bottom: 30, left: 90 },
-		width = 960 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom;
+// Set the dimensions and margins of the diagram
+var margin = { top: 20, right: 90, bottom: 30, left: 90 },
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
 
-	// append the svg object to the body of the page
-	// appends a 'group' element to 'svg'
-	// moves the 'group' element to the top left margin
-	var svg = d3.select("body").append("svg")
-		.attr("width", width + margin.right + margin.left)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate("
-			+ margin.left + "," + margin.top + ")");
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.right + margin.left)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate("
+		+ margin.left + "," + margin.top + ")");
 
-	var i = 0,
-		duration = 750,
-		root;
+var i = 0,
+	duration = 750,
+	root;
 
-	// declares a tree layout and assigns the size
-	var treemap = d3.tree().size([height, width]);
+// declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
 
-	// Assigns parent, children, height, depth
-	root = d3.hierarchy(treeData, function (d) { return d.children; });
-	root.x0 = height / 2;
-	root.y0 = 0;
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function (d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
 
-	// Collapse after the second level
-	root.children.forEach(collapse);
+// Collapse after the second level
+root.children.forEach(collapse);
 
-	update(root);
-}
+update(root);
 
 // Collapse the node and all it's children
 function collapse(d) {
@@ -204,7 +203,7 @@ fetch('flare-2.json').then(function (response) {
 			//products = json;
 			//initialize();
 			console.log(json);
-			main(json);
+			update(json);
 		});
 	} else {
 		console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
