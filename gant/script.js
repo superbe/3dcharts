467b7bb288879c43ded92fefff5290b70b556fc0
superbe <babarykin@list.ru>
2021-05-31 01:21:09 +0500

    Доработка

--- gant/script.js
+++ gant/script.js
@@ -1,3 +1,4 @@
+const padding = 50;
 
 // Определяем отступы для диаграммы
 var margin = { top: 20, right: 90, bottom: 30, left: 90 },
@@ -14,50 +15,48 @@ var svg = d3.select("body").append("svg")
 	.attr("transform", "translate("
 		+ margin.left + "," + margin.top + ")");
 
-
-
-// const width = 800;
-// const height = 600;
-// const padding = 50;
-// const innerWidth = width - padding * 2;
-// const innerHeight = height - padding * 2;
-
-// const svg = d3.select('svg').attr('width', width).attr('height', height);
+// Добавляем вспомогательные элементы в SVG
 const rootLayer = svg.append('g').attr('transform', `translate(${padding}, ${padding})`);
 const axisLayer = rootLayer.append('g');
 const xAxisLayer = axisLayer.append('g');
 const yAxisLayer = axisLayer.append('g');
 const tasksLayer = rootLayer.append('g');
 
+// Обновляем диаграмму.
 function update(series, xScale, yScale) {
   tasksLayer.selectAll('rect')
             .data(series)
             .enter()
             .append('rect')
             .attr('x', serie => xScale(serie.start))
-            .attr('y', serie => yScale(serie.name) + yScale.bandwidth() * 0.25)
+            //.attr('y', serie => yScale(serie.name) + yScale.bandwidth() * 0.25)
             .attr('width', serie => xScale(serie.end) - xScale(serie.start))
             .attr('height', yScale.bandwidth() * 0.5)
             .attr('fill', serie => serie.fill)
 }
 
+// Отрисовываем оси.
 function paintAxis(xScale, yScale) {
   xAxisLayer.call(d3.axisBottom().scale(xScale));
   yAxisLayer.call(d3.axisLeft().scale(yScale));
 }
 
+// Высчитываем масштаб по оси x.
 function calcXScale(series, xExtent, width) {
   return d3.scaleTime().range([0, innerWidth]).domain(xExtent);
 }
 
+// Высчитываем масштаб по оси y.
 function calcYScale(series, height) {
   return d3.scaleBand().range([0, innerHeight]).domain(series.map(serie => serie.name));
 }
 
+// Высчитываем диапазон значений по оси x.
 function calcExtent(series) {
   return [d3.min(series, serie => serie.start), d3.max(series, serie => serie.end)];
 }
 
+// Предобработка данных.
 function preProcess(jdata) {
 	return jdata.map(data => ({
 				name: data.name,
@@ -67,6 +66,7 @@ function preProcess(jdata) {
 			}));
 }
 
+// Отрисовали диаграмму.
 function buildGant(jdata) {
 	let series = preProcess(jdata);
 	let xExtent = calcExtent(series);
@@ -76,6 +76,7 @@ function buildGant(jdata) {
 	update(series, xScale, yScale);	
 }
 
+// Загрузили данные.
 fetch('data.json').then(function (response) {
 	if (response.ok) {
 		response.json().then(function (json) {
